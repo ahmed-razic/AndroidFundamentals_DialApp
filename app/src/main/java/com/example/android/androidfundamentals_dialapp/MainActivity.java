baea1d702c4f08e472dce1e2420cdf474a5201d7
ahmed-razic <ahmed.razic@gmail.com>
2021-02-09 13:42:44 +0100

    Added phone logic

--- app/src/main/java/com/example/android/androidfundamentals_dialapp/MainActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_dialapp/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.android.androidfundamentals_dialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editTextPhone);
+
+        if(editText != null){
+            editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if(actionId == EditorInfo.IME_ACTION_SEND) {
+                        dialNumber();
+                    } return true;
+                }
+            });
+        }
+    }
+
+    private void dialNumber() {
+
+        EditText editText = findViewById(R.id.editTextPhone);
+        String phoneNumber = null;
+
+        if(editText != null) {
+            phoneNumber = "tel: " + editText.getText().toString();
+        }
+        Log.d("Phone number", phoneNumber);
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(phoneNumber));
+        startActivity(intent);
     }
 }
